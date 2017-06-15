895d6c2c249d810e7d776f6a5a235e8c9a130731
todd ingalls <todd@toddingalls-AME.local>
2017-06-15 09:52:29 -0700

    working code with preset and pattr confirmed to work

--- source/pmenu/p2.maxpat
+++ source/pmenu/p2.maxpat
@@ -9,7 +9,7 @@
 			"modernui" : 1
 		}
 ,
-		"rect" : [ 59.0, 103.0, 640.0, 480.0 ],
+		"rect" : [ 59.0, 103.0, 905.0, 657.0 ],
 		"bglocked" : 0,
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
@@ -38,6 +38,121 @@
 		"subpatcher_template" : "",
 		"boxes" : [ 			{
 				"box" : 				{
+					"id" : "obj-19",
+					"maxclass" : "number",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 298.0, 417.0, 50.0, 22.0 ],
+					"style" : ""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-17",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 102.0, 122.0, 91.0, 22.0 ],
+					"style" : "",
+					"text" : "storagewindow"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-15",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 259.0, 86.0, 49.0, 22.0 ],
+					"style" : "",
+					"text" : "recall 2"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-12",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 214.0, 30.0, 47.0, 22.0 ],
+					"style" : "",
+					"text" : "store 2"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-13",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 4,
+					"outlettype" : [ "", "", "", "" ],
+					"patching_rect" : [ 106.0, 54.0, 58.0, 22.0 ],
+					"restore" : 					{
+						"_pmenu" : [ 230 ]
+					}
+,
+					"style" : "",
+					"text" : "autopattr",
+					"varname" : "u637000406"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-9",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 135.0, 85.0, 75.0, 22.0 ],
+					"saved_object_attributes" : 					{
+						"client_rect" : [ 1025, 44, 1466, 529 ],
+						"parameter_enable" : 0,
+						"storage_rect" : [ 933, 589, 1533, 889 ]
+					}
+,
+					"style" : "",
+					"text" : "pattrstorage",
+					"varname" : "u684000401"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-5",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 430.0, 393.0, 34.0, 22.0 ],
+					"style" : "",
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-10",
+					"linecount" : 2,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 415.0, 79.0, 150.0, 33.0 ],
+					"style" : "",
+					"text" : "0.3764705882 0.3843137255 0.4"
+				}
+
+			}
+, 			{
+				"box" : 				{
 					"id" : "obj-7",
 					"maxclass" : "message",
 					"numinlets" : 2,
@@ -55,7 +170,7 @@
 					"maxclass" : "newobj",
 					"numinlets" : 1,
 					"numoutlets" : 0,
-					"patching_rect" : [ 221.0, 396.0, 34.0, 22.0 ],
+					"patching_rect" : [ 221.0, 379.0, 34.0, 22.0 ],
 					"style" : "",
 					"text" : "print"
 				}
@@ -93,42 +208,79 @@
 					"numinlets" : 2,
 					"numoutlets" : 4,
 					"outlettype" : [ "dictionary", "", "", "" ],
-					"patching_rect" : [ 221.0, 231.0, 50.5, 22.0 ],
+					"patching_rect" : [ 221.0, 231.0, 159.0, 22.0 ],
 					"saved_object_attributes" : 					{
 						"embed" : 0,
 						"parameter_enable" : 0
 					}
 ,
 					"style" : "",
-					"text" : "dict"
+					"text" : "dict soundfiles pmenu6.json"
 				}
 
 			}
 , 			{
 				"box" : 				{
-					"bgcolor" : [ 0.322969, 0.31519, 0.325025, 1.0 ],
-					"color" : [ 0.614285, 0.654518, 0.758022, 1.0 ],
-					"elementcolor" : [ 0.82517, 0.78181, 0.059545, 1.0 ],
 					"fontface" : 0,
 					"fontname" : "Arial",
 					"fontsize" : 12.0,
 					"id" : "obj-1",
 					"maxclass" : "pmenu",
+					"my_string" : 					{
+
+					}
+,
 					"numinlets" : 1,
-					"numoutlets" : 1,
-					"outlettype" : [ "" ],
-					"patching_rect" : [ 221.0, 348.0, 209.696976, 20.0 ],
+					"numoutlets" : 3,
+					"outlettype" : [ "", "", "" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 221.0, 348.0, 210.0, 20.0 ],
 					"style" : "",
-					"text" : "The Dance of the Anemones.wav",
-					"textcolor" : [ 0.862745, 0.741176, 0.137255, 1.0 ]
+					"text" : "Neutron Star.wav",
+					"varname" : "_pmenu"
 				}
 
 			}
  ],
 		"lines" : [ 			{
 				"patchline" : 				{
+					"destination" : [ "obj-19", 0 ],
+					"source" : [ "obj-1", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-3", 0 ],
 					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-5", 0 ],
+					"source" : [ "obj-1", 2 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-9", 0 ],
+					"source" : [ "obj-12", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-9", 0 ],
+					"source" : [ "obj-15", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-9", 0 ],
+					"source" : [ "obj-17", 0 ]
 				}
 
 			}
--- source/pmenu/pmenu.c
+++ source/pmenu/pmenu.c
@@ -13,33 +13,37 @@
 #include "jpatcher_syms.h"
 #include "jgraphics.h"
 #include "ext_boxstyle.h"
-
-#define MAX_NUM_ITEMS 256
+#include "common/commonsyms.c"
 
 
 /**********************************************************************/
 // Data Structures
 typedef struct _pmenu {
 	t_jbox				j_box;
-    void*               pmenu_outlet;
+    void*               pmenu_item_outlet;
+    void*               pmenu_id_outlet;
+    void*               pmenu_data_outlet;
 	t_jrgba				j_textcolor;
 	t_jrgba				j_bgcolor;
     t_jrgba             j_highlightedtextcolor;
     t_jrgba             j_highlightedbgcolor;
     t_jpopupmenu*       j_menu;
-    t_hashtab*          hash_tab;
-    t_symbol*           keys[MAX_NUM_ITEMS];
-    long                numkeys;
+    t_hashtab*          item_hashtab;
+    t_hashtab*          data_hashtab;
+
     int                 id;
+    long                current_item;
+    t_dictionary        *dict;
+    
 } t_pmenu;
 
 
-typedef struct _trans {
+typedef struct _menu_info {
     t_pmenu *object;
     t_dictionary*   dict;
     t_jpopupmenu*   menu;
 
-} t_trans;
+} t_menu_info;
 
 /**********************************************************************/
 // Prototypes
@@ -53,6 +57,15 @@ void pmenu_dictionary(t_pmenu *x, t_symbol *s);
 
 void pmenu_attomarray_traverse(t_atom *a, void *x);
 void pmenu_dictionary_parse(t_pmenu *x, t_dictionary *dict, t_jpopupmenu *menu);
+t_max_err pmenu_notify(t_pmenu *x, t_symbol *s, t_symbol *msg, void *sender, void *data);
+void pmenu_jsave(t_pmenu *x, t_dictionary *d);
+void pmenu_int(t_pmenu *x, long n);
+void pmenu_open_dictionary(t_pmenu *x, t_dictionary *d);
+t_max_err pmenu_setvalueof(t_pmenu *x, long ac, t_atom *av);
+t_max_err pmenu_getvalueof(t_pmenu *x, long *ac, t_atom **av);
+void pmenu_preset(t_pmenu *x);
+void pmenu_assign(t_pmenu *x, long item, long notify);
+
 /**********************************************************************/
 // Globals and Statics
 
@@ -67,6 +80,7 @@ void ext_main(void *r)
 	t_class *c;
 
 	jpatcher_syms_init();
+    common_symbols_init();
 
 	c = class_new("pmenu",(method)pmenu_new,(method)pmenu_free,sizeof(t_pmenu),(method)NULL,A_GIMME,0L);
 
@@ -80,33 +94,37 @@ void ext_main(void *r)
     class_addmethod(c, (method)pmenu_mousedown,		"mousedown",	A_CANT, 0);
     class_addmethod(c, (method)pmenu_dictionary, 	"dictionary", 	A_SYM, 0);
     class_addmethod(c, (method)pmenu_assist,		"assist",		A_CANT, 0);
-	class_addmethod(c, (method)jbox_notify,			"notify",		A_CANT, 0);
+	class_addmethod(c, (method)pmenu_notify,			"notify",		A_CANT, 0);
+    class_addmethod(c, (method)pmenu_int, "int", A_LONG, 0);
+    class_addmethod(c, (method)pmenu_jsave,		"jsave",		A_CANT, 0);
+    class_addmethod(c, (method)pmenu_getvalueof, "getvalueof", A_CANT, 0);
+    class_addmethod(c, (method)pmenu_setvalueof, "setvalueof", A_CANT, 0);
+    class_addmethod(c, (method)pmenu_preset, "preset", 0);
+
+
+    CLASS_ATTR_STYLE_RGBA_PREVIEW(c, "highlightedbgcolor", 0, t_pmenu, j_highlightedbgcolor, "Highlighted Background Color", "rect_fill");
+    CLASS_ATTR_BASIC(c, "highlightedbgcolor", 0);
+    CLASS_ATTR_CATEGORY(c,		"highlightedbgcolor", 0, "Color");
+    CLASS_ATTR_DEFAULTNAME_SAVE_PAINT(c, "highlightedbgcolor", 0, ".3764705882 0.3843137255 .4 1.");
+
+    CLASS_ATTR_STYLE_RGBA_PREVIEW(c, "highlightedtext", 0, t_pmenu, j_highlightedtextcolor, "Highlighted Text Color", "rect_fill");
+    CLASS_ATTR_BASIC(c, "highlightedtext", 0);
+    CLASS_ATTR_CATEGORY(c,		"highlightedtext", 0, "Color");
+    CLASS_ATTR_DEFAULTNAME_SAVE_PAINT(c, "highlightedtext", 0, "1. 1. 1. 1.");
+    
+    CLASS_ATTR_STYLE_RGBA_PREVIEW(c,"bgfillcolor",0,t_pmenu,j_bgcolor,"Background Color","rect_fill");
+    CLASS_ATTR_BASIC(c, "bgfillcolor", 0);
+    CLASS_ATTR_CATEGORY(c,		"bgfillcolor", 0, "Color");
+    CLASS_ATTR_DEFAULTNAME_SAVE_PAINT(c, "bgfillcolor", 0, ".4 .4 .4 1.");
+    
+    CLASS_ATTR_RGBA(c, "textcolor", 0, t_pmenu, j_textcolor);
+    CLASS_ATTR_DEFAULT_SAVE_PAINT(c, "textcolor", 0, "1. 1. 1. 1.");
+    CLASS_ATTR_STYLE_LABEL(c, "textcolor", 0, "rgba", "Text Color");
+    CLASS_ATTR_CATEGORY(c, "textcolor", 0, "Color");
+    
 
     
-    CLASS_ATTR_SYM_VARSIZE(c,	"keys",		0, t_pmenu, keys, numkeys, MAX_NUM_ITEMS);
-    CLASS_ATTR_CATEGORY(c,		"keys",		0, "Dictionary");
-    CLASS_ATTR_LABEL(c,			"keys",		0, "Keys");
-    CLASS_ATTR_BASIC(c,			"keys",		0);
-    
-    
-    
-    
-    CLASS_ATTR_STYLE_RGBA_PREVIEW(c,"bgcolor",0,t_pmenu,j_bgcolor,"Background Color","rect_fill");
-    CLASS_ATTR_BASIC(c, "bgcolor", 0);
-    CLASS_ATTR_CATEGORY(c,		"bgcolor", 0, "Color");
-
-    
-    CLASS_ATTR_STYLE_RGBA_PREVIEW(c, "color", 0, t_pmenu, j_highlightedtextcolor, "Toggle Hilite Color", "rect_fill");
-    CLASS_ATTR_BASIC(c, "color", 0);
-    CLASS_ATTR_CATEGORY(c,		"color", 0, "Color");
-   
-    CLASS_ATTR_STYLE_RGBA_PREVIEW(c, "elementcolor", 0, t_pmenu, j_highlightedbgcolor, "Toggle Off Color", "rect_fill");
-    CLASS_ATTR_BASIC(c, "elementcolor", 0);
-    CLASS_ATTR_CATEGORY(c,		"elementcolor", 0, "Color");
-
-    
-    
-	CLASS_ATTR_DEFAULT(c, "rect", 0, "0. 0. 100. 12.");
+	CLASS_ATTR_DEFAULT(c, "rect", 0, "0. 0. 100. 22.");
 
 	class_register(CLASS_BOX, c);
 	s_pmenu_class = c;
@@ -132,35 +150,25 @@ t_pmenu *pmenu_new(t_symbol *name, short argc, t_atom *argv)
 				| JBOX_NODRAWBOX
 				| JBOX_DRAWINLAST
                 | JBOX_DRAWBACKGROUND
-				//| JBOX_TRANSPARENT
-				//		| JBOX_NOGROW
-						| JBOX_GROWY
-				//		| JBOX_GROWBOTH
-				//		| JBOX_IGNORELOCKCLICK
-				//		| JBOX_HILITE
-				//		| JBOX_BACKGROUND
-				//		| JBOX_NOFLOATINSPECTOR
+                | JBOX_GROWY
 				| JBOX_TEXTFIELD
 				;
 
 		jbox_new(&x->j_box, flags, argc, argv);
 		x->j_box.b_firstin = (t_object *) x;
-
-        x->j_highlightedtextcolor.red = .7; x->j_highlightedtextcolor.green =  0.7; x->j_highlightedtextcolor.blue = 0.7; x->j_highlightedtextcolor.alpha = 1.;
-        x->j_highlightedbgcolor.red = .1; x->j_highlightedbgcolor.green =  0.1; x->j_highlightedbgcolor.blue = 0.1; x->j_highlightedbgcolor.alpha = 1.;
         
-        
-        x->pmenu_outlet = outlet_new(x, NULL);
+        x->pmenu_data_outlet = outlet_new(x, NULL);
+        x->pmenu_id_outlet = outlet_new(x, NULL);
+        x->pmenu_item_outlet = outlet_new(x, NULL);
         
         x->j_menu = jpopupmenu_create();
         textfield = jbox_get_textfield((t_object *) x);
         
-
+        x->current_item = 1;
         if (textfield) {
             textfield_set_editonclick(textfield, 0);			// set it to 0 if you don't want user to edit it in lock mode
             textfield_get_textcolor(textfield, &x->j_textcolor );
             textfield_set_bgcolor(textfield,&x->j_bgcolor);
-           // cpost("%f %f %f %f", x->j_bgcolor.red, x->j_bgcolor.green, x->j_bgcolor.blue, x->j_bgcolor.alpha);
         }
         
         char *buff = " ";
@@ -168,34 +176,43 @@ t_pmenu *pmenu_new(t_symbol *name, short argc, t_atom *argv)
 
         textfield_set_useellipsis(jbox_get_textfield((t_object *)x),JGRAPHICS_TEXTLAYOUT_USEELLIPSIS);
         textfield_set_wordwrap(jbox_get_textfield((t_object *)x),JGRAPHICS_TEXTLAYOUT_NOWRAP );
-            
+        
+        jbox_processlegacydefaults((t_jbox *)x, d, 0);
         attr_dictionary_process(x, d);
 
-        x->hash_tab = (t_hashtab *)hashtab_new(0);
+        x->item_hashtab = (t_hashtab *)hashtab_new(0);
+        x->data_hashtab = (t_hashtab *)hashtab_new(0);
+        
+        t_object *s;
+        dictionary_getdictionary(d, gensym("my_string"), &s);
 
+        if (s) {
+            pmenu_open_dictionary(x, (t_dictionary*)s);
+
+        }
 		jbox_ready((t_jbox *)x);
+        
 	}
 	return x;
 }
 
 void pmenu_free(t_pmenu *x)
 {
-    if(x->j_menu) {
-        jpopupmenu_destroy(x->j_menu);
-    }
+    jpopupmenu_destroy(x->j_menu);
+    hashtab_clear(x->item_hashtab);
+    hashtab_clear(x->data_hashtab);
 	jbox_free((t_jbox *)x);
-    
+//    object_free(x->dict);
 }
 
 void pmenu_attomarray_traverse(t_atom *a, void *obj)
 {
     
-    t_pmenu *x = ((t_trans*)obj)->object;
-    //t_dictionary *d = ((t_trans*)obj)->dict;
-    t_jpopupmenu *m = ((t_trans*)obj)->menu;
+    t_pmenu *x = ((t_menu_info*)obj)->object;
+    t_jpopupmenu *m = ((t_menu_info*)obj)->menu;
 
     if(atomisdictionary(a)) {
-        t_trans o;
+        t_menu_info o;
         o.object = x;
         t_dictionary *dict = (t_dictionary*)atom_getobj(a);
         o.menu = m;
@@ -204,8 +221,7 @@ void pmenu_attomarray_traverse(t_atom *a, void *obj)
         ;//do nothing
         
     } else if(atomisstring(a)) {
-       // cpost("string %s", string_getptr((t_string *)atom_getobj(a)));
-        hashtab_store(x->hash_tab, (t_symbol*)x->id, atom_getobj(a));
+        hashtab_store(x->item_hashtab, (t_symbol*)(long)x->id, atom_getobj(a));
         jpopupmenu_additem(m, x->id++, string_getptr((t_string *)atom_getobj(a)), NULL, 0, 0, NULL);
 
     } else {
@@ -237,7 +253,7 @@ void pmenu_dictionary_parse(t_pmenu *x, t_dictionary *dict, t_jpopupmenu *menu)
 
         if(dictionary_entryisatomarray(dict, gensym("contents"))) {
             
-            t_trans o;
+            t_menu_info o;
             o.object = x;
             o.menu = jpopupmenu_create();
             const char *p;
@@ -247,42 +263,84 @@ void pmenu_dictionary_parse(t_pmenu *x, t_dictionary *dict, t_jpopupmenu *menu)
             dictionary_getatomarray((t_dictionary*)dict, gensym("contents"), &ap);
             atomarray_funall((t_atomarray*)ap,(method)pmenu_attomarray_traverse, (void*)&o);
                         jpopupmenu_setcolors(o.menu, x->j_textcolor,x->j_bgcolor,x->j_highlightedtextcolor,x->j_highlightedbgcolor);
-            t_jfont *font = jfont_create(jbox_get_fontname((t_object *)x)->s_name, jbox_get_font_slant((t_object *)x), jbox_get_font_weight((t_object *)x), jbox_get_fontsize((t_object *)x));
-            
+            t_jfont *font = jfont_create(jbox_get_fontname((t_object *)x)->s_name, (t_jgraphics_font_slant)jbox_get_font_slant((t_object *)x), (t_jgraphics_font_weight)jbox_get_font_weight((t_object *)x), jbox_get_fontsize((t_object *)x));
+
             jpopupmenu_setfont(o.menu, font );
             jfont_destroy(font);
             jpopupmenu_addsubmenu(menu, p, o.menu, 0);
+        }
+    } else if(dictionary_hasentry(dict, gensym("name")) && dictionary_hasentry(dict, gensym("data"))) {
+        if(!dictionary_entryisdictionary(dict, gensym("name")) || !dictionary_entryisatomarray(dict, gensym("name"))) {
+            t_symbol *a;
+            dictionary_getsym(dict, gensym("name"), &a);
+            hashtab_store(x->item_hashtab, (t_symbol*)(long)x->id, (t_object*)(string_new(a->s_name)));
+            hashtab_store(x->data_hashtab, (t_symbol*)(long)x->id, (t_object*)dict);
 
+            jpopupmenu_additem(menu, x->id++, a->s_name, NULL, 0, 0, NULL);
 
+            
         }
     }
+    
 }
 
-
-void pmenu_dictionary(t_pmenu *x, t_symbol *s)
+void pmenu_open_dictionary(t_pmenu *x, t_dictionary *d)
 {
-    t_dictionary *dict = dictobj_findregistered_clone(s);
+    x->dict = dictionary_clone(d);
     
     //top level of dict needs to have name and content keys
     //if there is a content key it must store and array
-    if(dictionary_hasentry(dict, gensym("name")) && dictionary_hasentry(dict, gensym("contents"))) {
-
-        if(dictionary_entryisatomarray(dict, gensym("contents"))) {
+    if(dictionary_hasentry(x->dict, gensym("name")) && dictionary_hasentry(x->dict, gensym("contents"))) {
+        
+        if(dictionary_entryisatomarray(x->dict, gensym("contents"))) {
             jpopupmenu_clear(x->j_menu);
-            hashtab_clear(x->hash_tab);
-            x->id = 1;
-            t_trans o;
+            hashtab_clear(x->item_hashtab);
+            x->id = 1; //
+            t_menu_info o;
             o.object = x;
-            o.dict = dict;
+            o.dict = x->dict;
             o.menu = x->j_menu;
             const char *p;
-            dictionary_getstring(dict, gensym("name"), &p);
-
+            dictionary_getstring(x->dict, gensym("name"), &p);
+            
             t_object *ap;
-            dictionary_getatomarray((t_dictionary*)dict, gensym("contents"), &ap);
-
+            dictionary_getatomarray((t_dictionary*)x->dict, gensym("contents"), &ap);
+            
             atomarray_funall((t_atomarray*)ap,(method)pmenu_attomarray_traverse, (void*)&o);
             
+            object_method(jbox_get_textfield((t_object *)x), gensym("settext"), p);
+            
+            jbox_redraw((t_jbox *)x);
+        }
+        
+    } else {
+        ;//do nothing
+    }
+}
+
+void pmenu_dictionary(t_pmenu *x, t_symbol *s)
+{
+    x->dict = dictobj_findregistered_clone(s);
+    
+    //top level of dict needs to have name and content keys
+    //if there is a content key it must store and array
+    if(dictionary_hasentry(x->dict, gensym("name")) && dictionary_hasentry(x->dict, gensym("contents"))) {
+
+        if(dictionary_entryisatomarray(x->dict, gensym("contents"))) {
+            jpopupmenu_clear(x->j_menu);
+            hashtab_clear(x->item_hashtab);
+            x->id = 1; //
+            t_menu_info o;
+            o.object = x;
+            o.dict = x->dict;
+            o.menu = x->j_menu;
+            const char *p;
+            dictionary_getstring(x->dict, gensym("name"), &p);
+
+            t_object *ap;
+            dictionary_getatomarray((t_dictionary*)x->dict, gensym("contents"), &ap);
+
+            atomarray_funall((t_atomarray*)ap,(method)pmenu_attomarray_traverse, (void*)&o);
             
             object_method(jbox_get_textfield((t_object *)x), gensym("settext"), p);
 
@@ -304,17 +362,14 @@ void pmenu_mousedown(t_pmenu *x,t_object *patcherview, t_pt pt, long modifiers)
     jpopupmenu_setcolors(x->j_menu, x->j_textcolor,x->j_bgcolor,x->j_highlightedtextcolor,x->j_highlightedbgcolor);
 
     
-    t_jfont *font = jfont_create(jbox_get_fontname((t_object *)x)->s_name, jbox_get_font_slant((t_object *)x), jbox_get_font_weight((t_object *)x), jbox_get_fontsize((t_object *)x));
+    t_jfont *font = jfont_create(jbox_get_fontname((t_object *)x)->s_name, (t_jgraphics_font_slant)jbox_get_font_slant((t_object *)x), (t_jgraphics_font_weight)jbox_get_font_weight((t_object *)x), jbox_get_fontsize((t_object *)x));
+
     
     jpopupmenu_setfont(x->j_menu, font );
     jfont_destroy(font);
-    int item = jpopupmenu_popup_belowrect(x->j_menu, rect, 1);
+    int item = jpopupmenu_popup_nearbox(x->j_menu, (t_object *)x, patcherview, (int)x->current_item);
     if(item > 0) {
-        t_object *o;
-        hashtab_lookup(x->hash_tab, (t_symbol*)item, &o);
-        object_method(jbox_get_textfield((t_object *)x), gensym("settext"), string_getptr((t_string *)o));
-        outlet_anything(x->pmenu_outlet, gensym(string_getptr((t_string *)o)), 0, NULL);
-        jbox_redraw((t_jbox *)x);
+        pmenu_assign(x, item, true);
     }
 }
 
@@ -339,32 +394,105 @@ void pmenu_assist(t_pmenu *x, void *b, long m, long a, char *s)
     else if (m==ASSIST_OUTLET) {
         switch (a) {
             case 0: strcpy(s, "selected item"); break;
-            case 1: strcpy(s, "data associated with item"); break;
+            case 1: strcpy(s, "index associated with item"); break;
+            case 2: strcpy(s, "data associated with item"); break;
         }
     }
 }
+
+void pmenu_int(t_pmenu *x, long item) {
     
+    if(item > 0 ) {
+        pmenu_assign(x, item, true);
+    }
+
+}
+t_max_err dictionary_appenddictionary(t_dictionary *d, t_symbol *key, t_object *value);
+
+void pmenu_jsave(t_pmenu *x, t_dictionary *d) {
     
+    dictionary_appenddictionary(d, gensym("my_string"), (t_object*)x->dict);
+
     
+}
+
+
+t_max_err pmenu_setvalueof(t_pmenu *x, long ac, t_atom *av)
+{
+    if (ac && av) {
+        if (av->a_type == A_LONG) {
+            pmenu_assign(x, (long)atom_getlong(av), false);
+        }
+    }
+    cpost("pmenu_setvalueof %ld",  atom_getlong(av));
+    return MAX_ERR_NONE;
+}
+//
+t_max_err pmenu_getvalueof(t_pmenu *x, long *ac, t_atom **av)
+{
+    t_atom a;
     
+    if (ac && av) {
+        if (*ac && *av) {
+            //memory passed in use it
+        } else {
+            *av = (t_atom *)getbytes(sizeof(t_atom));
+        }
+        *ac = 1;
+        atom_setlong(&a,(long)x->current_item);
+        **av = a;
+    }
+    cpost("pmenu_getvalueof %ld", (long)x->current_item);
     
-    //    t_rect rect;
-    //    printf("mouse down");
-    //    jbox_get_rect_for_view((t_object *)x, patcherview, &rect);
-    //    cpost("mouse down %f %f %f %f", x->j_bgcolor.red, x->j_bgcolor.green, x->j_bgcolor.blue, x->j_bgcolor.alpha);
-    //
-    //    jpopupmenu_setcolors(x->j_menu, x->j_textcolor,x->j_bgcolor,x->j_highlightedtextcolor,x->j_highlightedbgcolor);
-    //
-    //    t_jpopupmenu* sub = jpopupmenu_create();
-    //    jpopupmenu_additem(sub, 1, "xfoo", NULL, 0, 0, NULL);
-    //    jpopupmenu_additem(sub, 2, "xbar", NULL, 0, 0, NULL);
-    //    jpopupmenu_additem(sub, 3, "xbaz", NULL, 0, 0, NULL);
-    //
-    //
-    //    jpopupmenu_clear(x->j_menu);
-    //    jpopupmenu_additem(x->j_menu, 1, "foo", NULL, 0, 0, NULL);
-    //    jpopupmenu_additem(x->j_menu, 2, "bar", NULL, 0, 0, NULL);
-    //    jpopupmenu_addsubmenu(x->j_menu, "sub", sub, 0);
-    //    jpopupmenu_additem(x->j_menu, 3, "baz", NULL, 0, 0, NULL);
-    //    post("%i",jpopupmenu_popup_belowrect	(x->j_menu, rect, 1));
-    //    jpopupmenu_destroy(sub);
+    return MAX_ERR_NONE;
+}
+
+
+void pmenu_assign(t_pmenu *x, long item, long notify)
+{
+    if(item > 0) {
+        t_object *o;
+        hashtab_lookup(x->item_hashtab, (t_symbol*)(long)item, &o);
+        object_method(jbox_get_textfield((t_object *)x), gensym("settext"), string_getptr((t_string *)o));
+        
+        t_object *od;
+        
+        long result = hashtab_lookup(x->data_hashtab, (t_symbol*)(long)item, &od);
+        
+        if (!result) {
+            
+            long       ac = 0;
+            t_atom     *av = NULL;
+            t_max_err  err;
+            err = dictionary_copyatoms((t_dictionary*)od, gensym("data"), &ac, &av);
+            if (!err && ac && av) {
+                dictobj_outlet_atoms(x->pmenu_data_outlet,ac,av);
+            }
+            if (av)
+                sysmem_freeptr(av);
+        }
+        
+        outlet_int(x->pmenu_id_outlet, item);
+        outlet_anything(x->pmenu_item_outlet, gensym(string_getptr((t_string *)o)), 0, NULL);
+        x->current_item = item;
+        if (notify) {
+            object_notify(x, _sym_modified, NULL);
+        }
+       
+        jbox_redraw((t_jbox *)x);
+    }
+}
+
+
+void pmenu_preset(t_pmenu *x)
+{
+    preset_int((t_object*)x, x->current_item);
+}
+
+
+t_max_err pmenu_notify(t_pmenu *x, t_symbol *s, t_symbol *msg, void *sender, void *data) {
+
+   // cpost("notif %s", msg->s_name);
+    //jbox_redraw((t_jbox *)x);
+    return jbox_notify((t_jbox *)x, s, msg, sender, data);
+}
